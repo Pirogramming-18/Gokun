d75c1188af21882abe13a13407213613288be5a8
kuny092 <kokun12@naver.com>
2023-01-06 03:00:04 +0900

    unit38 퀴즈

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -3,25 +3,27 @@ a=input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능):')
 a=float(a)
 a=int(a)
 
-if a < 0:
-    print ("정수를 입력하세요")
-elif a > 3:
-    print ("1,2,3 중 하나를 입력하세요")
+while a < 0:
+        print ("정수를 입력하세요")
+while a > 3:
+        print ("1,2,3 중 하나를 입력하세요")
 else:
     for _ in range(a):
         print ('playerA:',num)
         num += 1
-    
+
 b=input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능):')
 b=float(b)
 b=int(b)
 
 if b < 0:
     print ("정수를 입력하세요")
-elif a > 3:
+elif b > 3:
     print ("1,2,3 중 하나를 입력하세요")
 else:
     for _ in range(b):
         print ('playerB:',num)
         num += 1
 
+
+    
