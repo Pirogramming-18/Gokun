60792593c66a183ad60d0c758cdf5159ad21e291
kuny092 <kokun12@naver.com>
2023-01-06 01:58:59 +0900

    1-5

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,10 +1,27 @@
-num = 0
+num = 1
 a=input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능):')
 a=float(a)
 a=int(a)
+
 if a < 0:
     print ("정수를 입력하세요")
 elif a > 3:
     print ("1,2,3 중 하나를 입력하세요")
-for num in range(a):
-    print ('playerA:',num)
+else:
+    for _ in range(a):
+        print ('playerA:',num)
+        num += 1
+    
+b=input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능):')
+b=float(b)
+b=int(b)
+
+if b < 0:
+    print ("정수를 입력하세요")
+elif a > 3:
+    print ("1,2,3 중 하나를 입력하세요")
+else:
+    for _ in range(b):
+        print ('playerB:',num)
+        num += 1
+
