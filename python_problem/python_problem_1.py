10ab1baeb5d861bc2dc2f5c8081f1b869151bbca
kuny092 <kokun12@naver.com>
2023-01-06 03:36:54 +0900

    unit44 퀴즈

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,29 +1,22 @@
-num = 1
-a=input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능):')
-a=float(a)
-a=int(a)
-
-while a < 0:
-        print ("정수를 입력하세요")
-while a > 3:
-        print ("1,2,3 중 하나를 입력하세요")
-else:
-    for _ in range(a):
-        print ('playerA:',num)
-        num += 1
-
-b=input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능):')
-b=float(b)
-b=int(b)
-
-if b < 0:
-    print ("정수를 입력하세요")
-elif b > 3:
-    print ("1,2,3 중 하나를 입력하세요")
-else:
-    for _ in range(b):
-        print ('playerB:',num)
-        num += 1
-
-
-    
+turn = 'A' # 플레이어 출력 시 사용
+num = 0
+end = False # while문 종료조건
+while not end:
+    inp = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능):')
+    try : 
+        inp = int(inp) # 실수면 오류
+        if inp < 0 : raise Exception() # 0보다 작아도 오류
+        if (0 < inp < 4): # 1,2,3일때만
+            while inp > 0:
+                num += 1
+                if num == 31:
+                    end = True
+                    inp = 0
+                inp -= 1
+                print("player{} : {}".format(turn, num))
+            turn = ('B' if turn=='A' else 'A')
+        else: # 0이거나 4 이상이면
+            print("1,2,3 중 하나를 입력하세요")
+    except:
+        print("정수를 입력하세요")
+        
