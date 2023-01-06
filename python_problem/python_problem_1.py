43f8d117e70d1a68e08eebc0f2512c7e00f514f1
kuny092 <kokun12@naver.com>
2023-01-07 00:22:57 +0900

    1-8

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,6 +1,8 @@
+import random
+
 num=0
 player='B'
-end = False
+
 try:
     while True:
                 a=int(input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능):'))
@@ -8,7 +10,7 @@ try:
                         print ('정수를 입력하세요')
                 elif a>3:
                         print ('1,2,3 중 하나를 입력하세요')
-                player = ('B' if player=='A' else 'A')
+                player = ('A' if player=='B' else 'B')
                 if (0<a<4):
                  for _ in range(a):  
                         if num == 31:
