2d5a43c86924b2c3cec3dd0556d0ca197b3eaab2
kuny092 <kokun12@naver.com>
2023-01-07 00:32:26 +0900

    1-9

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -4,24 +4,47 @@ def brGame():
     pc=1
     #0: com 1: player
 
-try:
     while True:
-                a=int(input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능):'))
-                if a<=0:
-                        print ('정수를 입력하세요')
-                elif a>3:
-                        print ('1,2,3 중 하나를 입력하세요')
-                player = ('A' if player=='B' else 'B')
-                if (0<a<4):
-                 for _ in range(a):  
-                        if num == 31:
-                                print ('player{0} 승'.format(player))
-                                break
-                        num += 1
-                        print ('player{0}:{1}'.format(player, num))
-
-
-except Exception:
-      print ('정수를 입력하세요')
+        if pc == 0:
+            count=R.randint(1,3)
+        elif pc == 1:
+            try:
+                count=int(input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능):'))
+                if not 1<=count<=3:
+                    raise Exception('1,2,3 중 하나를 입력하세요')
+            except ValueError:
+                    print ('정수를 입력하세요')
+                    continue
+            except Exception as e:
+                    print (e)
+                    continue
+                    #입력 끝
+                        
+        if pc==0:
+            pc=1
+            count_all=count+num
+            while num<count_all:
+                num+=1
+                print ('computer:',num)
+                if num>=31:
+                    return pc
+                    #컴퓨터 끝
+                
+        elif pc==1:
+            pc=0
+            count_all=count+num
+            while num<count_all:
+                num+=1
+                print ('player:',num)
+                if num>=31:
+                    return pc
+                    #사람일때 끝
+         
+br=brGame()
+if br==0:
+    print ('computer win!')
+elif br==1:
+    print ('player win!')
+                    
 
 
